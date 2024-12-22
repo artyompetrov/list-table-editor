e9ce26570f5b458d91af03d130231b083ac6a2b2
Artem Petrov <apetrov@ptsecurity.com>
2024-12-22 18:36:20 +0700

    увеличил макс ширину

--- media/tableEditor.js
+++ media/tableEditor.js
@@ -30384,7 +30384,7 @@ function sanitizeHTML(value) {
             headerSort: false, resizable: false, minWidth: 30, rowHandle: true, formatter: "rownum"
         },
         columnDefaults: {
-            maxWidth: 1000,
+            maxWidth: 700,
             editor: "textarea",
             headerSort: false,
             formatter: /*"textarea" */ customTextAreaFormatter,
